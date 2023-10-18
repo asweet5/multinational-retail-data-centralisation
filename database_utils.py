95f85d5f0f6fa805d5138bd83fcc2f647532a399
asweet5 <alexanderjsweet@gmail.com>
2023-10-18 18:22:38 +0100

    Creates read_db_creds method

--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,11 @@
-class DatabaseConnector(self):
+import yaml
+
+class DatabaseConnector:
+    def __init__(self, creds):
+        self.creds = creds
+
+    def read_db_creds(self):
+        with open(self.creds, 'r') as creds:
+            data = yaml.safe_load(creds)
+            return data
+        
